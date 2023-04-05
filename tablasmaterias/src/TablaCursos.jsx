338c77ae58604adf83a8cae2c048fa8d79f909bd
BryamDigar <bryamdiaz1010@gmail.com>
2023-04-05 09:19:57 -0500

    Se crea proyecto tablasmaterias

---
+++ tablasmaterias/src/TablaCursos.jsx
@@ -0,0 +1,30 @@
+
+import React from 'react'
+
+const courseList = [{
+    id: 1,
+    nombre: 'Calculo vertorial'
+  },
+  {
+    id: 2,
+    nombre: 'Programación de nuevas tecnologias'
+  },{
+    id: 3,
+    nombre: 'Fisica Electricidad y Magnetismo'
+  },{
+    id: 4,
+    nombre: 'Logica Digital'
+  },{
+    id: 5,
+    nombre: 'Core Curriculum Persona y Cultura I'
+  },{
+    id: 6,
+    nombre: 'Ingles'
+  }]
+  
+
+export const TablaCursos = () => {
+  return (
+    <div>TablaCursos</div>
+  )
+}
---
+++ tablasmaterias/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client';
+import { TablaCursos } from './TablaCursos';
+
+
+ReactDOM.createRoot( document.getElementById('root') ).render(
+    <React.StrictMode>
+        <TablaCursos/>
+    </React.StrictMode>
+)
