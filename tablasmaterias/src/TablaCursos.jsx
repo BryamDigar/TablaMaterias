2a5b513230484fcd20aa50f8fd950c0ae45b93bb
juanMS <285juanda@gmail.com>
2023-04-05 14:36:09 -0500

    Se acaba la creación del diseño de la tabla y las acciones

--- tablasmaterias/src/TablaCursos.jsx
+++ tablasmaterias/src/TablaCursos.jsx
@@ -17,22 +17,52 @@ const courseList = [
 export class TablaCursos extends React.Component{
 
   state={
-    courseList: courseList
+    courseList: courseList,
+    form:{
+      id:" ",
+      nombre: " ",
+    },
+    modalInsertar: false,
+    modalEditar: false,
+  }
+
+  handleChange=e=>{
+    this.setState({
+      form:{
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      }
+    });
+  }
+
+  mostrarModalInsertar=()=>{
+    this.setState({modalInsertar: true});
+  }
+
+  mostrarModalEditar=()=>{
+    this.setState({modalEditar: true});
+  }
+
+
+  ocultarModalInsertar=()=>{
+    this.setState({modalInsertar: false});
+  }
+
+  ocultarModalEditar=()=>{
+    this.setState({modalEditar: false});
   }
 
   render(){
     return (
       <>
       <Container>
+
       <br />
-      <Button color="success">Insertar Nuevo Curso</Button>{"   "}
-      <Button color="danger">Eliminar Curso</Button>{"   "}
-      <Button color="primary">Editar Curso</Button>{"   "}
+      <Button color="success" onClick={()=>this.mostrarModalInsertar()}>Insertar Nuevo Curso</Button>{"   "}
       <br /><br />
-      </Container>
   
       <Table>
-        <thead><tr><th>Id</th>
+        <thead><tr><th>ID</th>
         <th>Nombre</th>
         <th>Acciones</th>
         </tr></thead>
@@ -41,17 +71,69 @@ export class TablaCursos extends React.Component{
             <tr>
               <td>{elemento.id}</td>
               <td>{elemento.nombre}</td>
+              <td><Button color="danger">Eliminar</Button></td>
+              <td><Button color="primary" onClick={()=>this.mostrarModalEditar()}>Editar</Button></td>
             </tr>
           ))}
-  
         </tbody>
+    </Table>
+
+    </Container>
+    
+    <Modal id="botonAgregarCurso" isOpen= {this.state.modalInsertar}>
+        <ModalHeader>
+          <div>
+            <h3>Crear nuevo curso:</h3> 
+          </div>
+        </ModalHeader>          
+
+        <ModalBody>
+            <FormGroup>
+              <label>ID:</label>
+              <input className="form" readOnly type="text" value={this.state.courseList.length+1}/>
+            </FormGroup>
+
+            <FormGroup>
+              <label>Nombre del curso:</label>
+              <input className="form" name="nombre" type="text" onChange={this.handleChange} />
+            </FormGroup>
+        </ModalBody>
+
+        <ModalFooter>
+            <Button color="primary">Insertar</Button>
+            <Button color="danger" onClick={()=>this.ocultarModalInsertar()}>Cancelar</Button>
+        </ModalFooter>
+
+    </Modal>
+
+    <Modal id="botonEditarPantalla1" isOpen= {this.state.modalEditar}>
+        <ModalHeader>
+          <div>
+            <h3>Digita el ID del curso a editar:</h3> 
+          </div>
+        </ModalHeader>          
+
+        <ModalBody>
+            <FormGroup>
+              <label>ID:</label>
+              {"   "}<input className="form" name="id" type="text" onChange={this.handleChange} />
+            </FormGroup>
+
+            <FormGroup>
+              <label>Nombre:</label>
+              {"   "}<input className="form" name="nombre" type="text" onChange={this.handleChange} />
+            </FormGroup>
+        </ModalBody>
+
+        <ModalFooter>
+            <Button color="primary">Insertar</Button>
+            <Button color="danger" onClick={()=>this.ocultarModalEditar()}>Cancelar</Button>
+        </ModalFooter>
+    </Modal>    
+
+  </>
+  );
   
-      </Table>
-  
-      </>)
   }
 
 }
-
-
-
